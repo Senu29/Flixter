5bf56dd47da23b5f6bc20ed5481af2e55ea38efc
Senuda Ratnayake <tdr5250@psu.edu>
2021-02-24 13:55:39 +0530

    Completed the Required User Stories for the Assignment 2 Flixter part 2.

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Senuda Ratnayake on 2/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
